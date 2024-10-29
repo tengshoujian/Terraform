a4c621f2712add02c7012f796954c4db70004eaf
tengsj <tengshoujian@163.com>
2024-10-29 17:40:55 +0800

    delete vpc and subnet

--- main.tf
+++ main.tf
@@ -11,17 +11,17 @@ provider "aws" {
   region = "cn-north-1"
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "192.18.0.0/16"
-}
+# # Create a VPC
+# resource "aws_vpc" "example" {
+#   cidr_block = "192.18.0.0/16"
+# }
 
-#create a subnet
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.example.id
-  cidr_block = "192.18.1.0/24"
+# #create a subnet
+# resource "aws_subnet" "main" {
+#   vpc_id     = aws_vpc.example.id
+#   cidr_block = "192.18.1.0/24"
 
-  tags = {
-    Name = "Main"
-  }
-}
+#   tags = {
+#     Name = "Main"
+#   }
+# }
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "vpc_id" {
-  value = aws_vpc.example.id
-}
+# output "vpc_id" {
+#   value = aws_vpc.example.id
+# }
 
-output "vpc_arn" {
-  value = aws_vpc.example.arn
-}
+# output "vpc_arn" {
+#   value = aws_vpc.example.arn
+# }
 
-output "vpc_cidr" {
-  value = aws_vpc.example.cidr_block
-}
+# output "vpc_cidr" {
+#   value = aws_vpc.example.cidr_block
+# }
